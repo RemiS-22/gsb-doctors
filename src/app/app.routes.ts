33219bce7b2cc3581cf8fd89a9534f4b9210feb7
Rémi SIMONETTO <rsimonetto@turgot-paris.si>
2025-11-18 11:30:37 +0100

    feat(app): Add routes in router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DoctorsPageComponent } from './pages/doctors-page/doctors-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'medecins', component: DoctorsPageComponent },
+    { path: '', component: DoctorsPageComponent }, // route racine  
+    { path: '**', redirectTo: '' },
+];
